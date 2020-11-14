064b6d9550c819f7a08c4813a5849e97f7ead966
Prashanth-Nandamuri <prashanth.nandamuri@gmail.com>
2020-11-13 20:00:20 -0500

    added media queries for tagcloud

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-window.onload = function generateSkillsCloud() {
-	var texts = [
-	'Angular', 'Bootstrap', 'JavaScript',
-	'CSS', 'HTML', 'AJAX',
-	'JSON', 'JQuery', 'less',
-	'Grunt', 'MySQL', '508 Compliance',
-	'REST', 'MVC', 'Github', 
-	'SVN', 'SpringBoot', 'npm',
-	'TypeScript', 'RxJs',
-	];
+var texts = [
+'Angular', 'Bootstrap', 'JavaScript',
+'CSS', 'HTML', 'AJAX',
+'JSON', 'JQuery', 'less',
+'Grunt', 'MySQL', '508 Compliance',
+'REST', 'MVC', 'Github', 
+'SVN', 'SpringBoot', 'npm',
+'TypeScript', 'RxJs',
+];
+var flag = false;
+function callTag(rad){
 	var tagCloud = TagCloud('.content', texts,{
 	    // radius in px
-	    radius: 275,
+	    radius: rad,
 	    // animation speed
 	    // slow, normal, fast
 	    maxSpeed: 'slow',
@@ -22,7 +23,9 @@ window.onload = function generateSkillsCloud() {
 	    // interact with cursor move on mouse out
 	    keep: false
 	});
-
+}
+window.onload = function generateSkillsCloud() {
+	callTag(275);
 	var mybutton = document.getElementById("goToTop");
 	// When the user scrolls down 20px from the top of the document, show the button
 	window.onscroll = function() {scrollFunction()};
@@ -38,3 +41,17 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+function displayWindowSize(){
+	var w = document.documentElement.clientWidth;
+	var h = document.documentElement.clientHeight;
+	if(w < 1000 && flag == false) {
+		document.getElementById('abcd').innerHTML = "";
+		flag = true;
+		callTag(100);
+	} else if( w > 1000 && flag == true) {
+		document.getElementById('abcd').innerHTML = "";
+		flag = false;
+		callTag(275);
+	}
+}
+window.addEventListener("resize", displayWindowSize);
